b035811189e7f05d40cac99dcd5eea3e43760fad
tejus <thequantumcoder@gmail.com>
2024-01-29 21:51:06 +0530

    create landing page and configure themes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 export default function Home() {
   return (
     <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center">
-      <div>
-        Hello World
+      <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
+        Quill is now public. 
       </div>
     </MaxWidthWrapper>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      maxWidth: {
+        "8xl": "1408px"
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
