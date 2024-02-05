dd1761179b97aa64b58f1aa8538af320cd7ab7c7
tejus <thequantumcoder@gmail.com>
2024-02-05 08:12:02 +0530

    build determinate progress bar

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -1,15 +1,100 @@
 "use client";
 
 import { Dialog, DialogTrigger } from "@radix-ui/react-dialog";
+import { Cloud, File } from "lucide-react";
 import { useState } from "react";
+import Dropzone from "react-dropzone";
 import { Button } from "./ui/button";
 import { DialogContent } from "./ui/dialog";
+import { Progress } from "./ui/progress";
+
+const UploadDropZone = () => {
+
+  const [isUploading, setIsUploading] = useState(true);
+  const [uploadProgress, setUploadProgress] = useState(0);
+
+  const startStimulatedProgress = () => {
+    setUploadProgress(0);
+    const interval = setInterval(() => {
+      setUploadProgress(prevProgress => {
+        if (prevProgress >= 95) {
+          clearInterval(interval);
+          return prevProgress;
+        }
+        else {
+          return prevProgress + 5;
+        }
+      })
+    }, 500)
+    
+    return interval;
+  }
+
+  return (
+    <Dropzone multiple={false} onDrop={async(acceptedFile) => {
+      setIsUploading(true);
+
+      const progressInterval = startStimulatedProgress();
+
+      // handle file uploading
+      await new Promise(resolve=>setTimeout(() => {
+        resolve
+      }, 3000))
+
+      clearInterval(progressInterval);
+
+      setUploadProgress(100);
+    }}>
+      {
+        ({getRootProps,getInputProps,acceptedFiles}) => (
+          <div {...getRootProps()} className="border h-64 m-4 border-dashed border-gray-300 rounded-lg">
+            <div className="flex justify-center items-center h-full w-full">
+              <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
+                <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                  <Cloud className="h-6 w-6 text-zinc-500 mb-2" />
+                  <p className="mb-2 text-sm text-zinc-700">
+                    <span className="font-semibold">
+                      Click to upload
+                    </span> {''}
+                    or drag and drop
+                  </p>
+                  <p className="text-xs text-zinc-500">PDF (up to 4MB)</p>
+                </div>
+
+                {
+                  acceptedFiles && acceptedFiles[0] ? (
+                    <div className="max-w-xs bg-white flex items-center rounded-md overflow-hidden outline outline-[1px] outline-zinc-200 divide-x divide-zinc-200">
+                      <div className="px-3 py-2 h-full place-items-center grid">
+                        <File className="h-4 w-4 text-blue-500" />
+                      </div>
+                      <div className="px-3 py-2 h-full text-sm truncate">
+                        {
+                          acceptedFiles[0].name
+                        }
+                      </div>
+                    </div>
+                  ) : null
+                }
+
+                {
+                  isUploading ? (
+                    <div className="w-full mt-4 max-w-xs mx-auto">
+                      <Progress value={uploadProgress} className="h-1 w-full bg-zinc-200"/>
+                    </div>
+                  ) : null
+                }
+              </label>
+            </div>
+          </div>
+        )
+      }
+    </Dropzone>
+  )
+}
 
 const UploadButton = () => {
 
   const [isOpen, setIsOpen] = useState(false);
-
-
 
   return (
     <Dialog open={isOpen} onOpenChange={(v) => {
@@ -23,7 +108,7 @@ const UploadButton = () => {
         </Button>
       </DialogTrigger>
       <DialogContent>
-        content
+        <UploadDropZone/>
       </DialogContent>
     </Dialog>
   )
