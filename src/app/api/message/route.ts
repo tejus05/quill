8cb12b8aae467046c44215dc498ae285badfebbb
tejus <thequantumcoder@gmail.com>
2024-02-06 21:13:49 +0530

    set up context api

---
+++ src/app/api/message/route.ts
@@ -0,0 +1,18 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextRequest } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+  const body = await req.json();
+
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  const userId = user?.id;
+
+  if (!userId) {
+    return new Response("Unauthorized", {status: 401})
+  }
+
+  
+
+}
--- src/components/chat/ChatContext.tsx
+++ src/components/chat/ChatContext.tsx
@@ -1,15 +1,64 @@
-import { createContext } from "react"
+import React, { ReactNode, createContext, useState } from "react"
+import { useToast } from "../ui/use-toast";
+import { useMutation } from "@tanstack/react-query";
 
 type StreamResponse = {
   addMessage():void,
   message:string,
-  handleInputChange():void,
+  handleInputChange(event: React.ChangeEvent<HTMLTextAreaElement>):void,
   isLoading:boolean
 };
 
-export const ChatContext = createContext({
+interface Props{
+  fileId: string,
+  children: ReactNode
+}
+
+export const ChatContext = createContext<StreamResponse>({
   addMessage: () => { },
   message: "",
   handleInputChange: () => { },
   isLoading: false
 })
+
+export const ChatContextProvider = ({fileId, children}:Props) => {
+  const [message, setMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { toast } = useToast();
+
+  const { mutate: sendMessage } = useMutation({
+    mutationFn: async ({message}:{message:string}) => {
+      const response = await fetch("/api/message", {
+        method: "POST",
+        body: JSON.stringify({
+          fileId,
+          message
+        })
+      })
+
+      if (!response.ok) {
+        throw new Error("Failed to send message")
+      }
+      
+      return response.body
+    }
+  }); 
+
+  const addMessage = () => sendMessage({ message })
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMessage(e.target.value)
+  };
+
+  return (
+    <ChatContext.Provider value={{
+      addMessage,
+      message,
+      handleInputChange,
+      isLoading
+    }}>
+      {children}
+    </ChatContext.Provider>
+  )
+}
