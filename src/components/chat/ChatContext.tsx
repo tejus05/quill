ee3a3983c6a3d134341f427c48cc5830f9a9b144
tejus <thequantumcoder@gmail.com>
2024-02-09 21:22:07 +0530

    finish optimistic updates

--- src/components/chat/ChatContext.tsx
+++ src/components/chat/ChatContext.tsx
@@ -1,6 +1,8 @@
-import React, { ReactNode, createContext, useState } from "react"
+import React, { ReactNode, createContext, useRef, useState } from "react"
 import { useToast } from "../ui/use-toast";
 import { useMutation } from "@tanstack/react-query";
+import { trpc } from "@/app/_trpc/client";
+import { INFINITE_QUERY_LIMIT } from "@/config/infinite-query";
 
 type StreamResponse = {
   addMessage():void,
@@ -27,6 +29,10 @@ export const ChatContextProvider = ({fileId, children}:Props) => {
 
   const { toast } = useToast();
 
+  const utils = trpc.useUtils();
+
+  const backupMessage = useRef("");
+
   const { mutate: sendMessage } = useMutation({
     mutationFn: async ({message}:{message:string}) => {
       const response = await fetch("/api/message", {
@@ -41,7 +47,152 @@ export const ChatContextProvider = ({fileId, children}:Props) => {
         throw new Error("Failed to send message")
       }
       
-      return response.json();
+      return response.body;
+    },
+    onMutate: async({message}) => {
+      backupMessage.current = message;
+      setMessage("");
+
+      // step 1
+      await utils.getFileMessages.cancel();
+
+      // step 2
+      const previousMessages = utils.getFileMessages.getInfiniteData();
+
+      // step 3
+      utils.getFileMessages.setInfiniteData({
+        fileId, limit: INFINITE_QUERY_LIMIT
+      },
+        (old) => {
+          if (!old) {
+            return {
+              pages: [],
+              pageParams: []
+            }
+          }
+
+          let newPages = [...old.pages]
+
+          let latestPage = newPages[0]!
+
+          latestPage.messages = [
+            {
+              createdAt: new Date().toISOString(),
+              id: crypto.randomUUID(),
+              text: message,
+              isUserMessage: true
+            },
+            ...latestPage.messages
+          ]
+
+          newPages[0] = latestPage;
+
+          return {
+            ...old,
+            pages: newPages
+          }
+        }
+      )
+
+      setIsLoading(true)
+
+      return {
+        previousMessages: previousMessages?.pages.flatMap((page)=>page.messages) ?? []
+      }
+    },
+    onSuccess: async(stream) => {
+      setIsLoading(false);
+
+      if (!stream) {
+        return toast({
+          title: "There was a problem sending this message",
+          description: "Please refresh this page and try again",
+          variant: "destructive",
+        });
+      }
+      const reader = stream.getReader();
+      const decoder = new TextDecoder();
+      let done = false;
+
+      // accumulated response 
+      let accumulatedResponse = "";
+      while (!done) {
+        const { value, done: doneReading } = await reader.read();
+        done = doneReading;
+        const chunkValue = decoder.decode(value);
+
+        accumulatedResponse += chunkValue; 
+
+        // append the chunk to the actual message
+
+        utils.getFileMessages.setInfiniteData(
+          { fileId, limit: INFINITE_QUERY_LIMIT },
+          (old) => {
+            if (!old) {
+              return {
+                pages: [],
+                pageParams: []
+              }
+            }
+            let isAiResponseCreated = old.pages.some((page)=>page.messages.some((message)=>message.id === "ai-response"))
+
+            let updatedPages = old.pages.map(page => {
+              if (page === old.pages[0]) {
+                let updatedMessages;
+
+                if (!isAiResponseCreated) {
+                  updatedMessages = [
+                    {
+                      createdAt: new Date().toISOString(),
+                      id: "ai-response",
+                      text: accumulatedResponse,
+                      isUserMessage: false
+                    },
+                    ...page.messages
+                  ]
+                }
+                else {
+                  updatedMessages = page.messages.map(message => {
+                    if (message.id === "ai-response") {
+                      return {
+                        ...message,
+                        text: accumulatedResponse
+                      }
+                    }
+                    return message; 
+                  })
+                }
+
+                return {
+                  ...page,
+                  messages: updatedMessages
+                }
+              }
+
+              return page;
+            })
+
+            return {
+              ...old,
+              pages: updatedPages
+            }
+          }
+        )
+      }
+    },
+    onError: (_, __, context) => {
+      setMessage(backupMessage.current);
+      utils.getFileMessages.setData(
+        { fileId },
+        {messages: context?.previousMessages ?? []}
+      )
+    },
+    onSettled: async() => {
+      setIsLoading(false);
+
+      await utils.getFileMessages.invalidate({
+        fileId
+      });
     }
   }); 
 
--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -6,6 +6,8 @@ import Message from "./Message";
 import { useContext, useEffect, useRef } from "react";
 import { ChatContext } from "./ChatContext";
 import { keepPreviousData } from "@tanstack/react-query";
+import { useIntersection } from "@mantine/hooks";
+
 
 interface MessagesProps {
   fileId: string;
@@ -46,6 +48,16 @@ const Messages = ({ fileId }: MessagesProps) => {
 
   const lastMessageRef = useRef<HTMLDivElement>(null);
 
+  const { ref, entry } = useIntersection({
+    root: lastMessageRef.current,
+    threshold: 1
+  })
+
+  useEffect(() => {
+    if (entry?.isIntersecting) {
+      fetchNextPage();
+    }
+  },[entry,fetchNextPage])
 
   return (
     <div className="flex max-h-[calc(100vh-3.5rem-7rem)] border-zinc-200 flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
@@ -61,6 +73,7 @@ const Messages = ({ fileId }: MessagesProps) => {
                 message={message}
                 isNextMessageSamePerson={isNextMessageSamePerson}
                 key={message.id}
+                ref={ref}
               />
             );
           } else
