9da2d4ed75a2b0a40da3afaae405f2b69c4dd685
tejus <thequantumcoder@gmail.com>
2024-02-09 09:29:25 +0530

    display messages

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -9,14 +9,14 @@ interface ChatInputProps {
 }
 
 const ChatInput = ({ isDisabled }: ChatInputProps) => {
+  const { addMessage, handleInputChange, isLoading, message } =
+    useContext(ChatContext);
 
   const textareaRef = useRef<HTMLTextAreaElement>(null);
 
-  const { addMessage, message, handleInputChange, isLoading } = useContext(ChatContext);
-
   return (
     <div className="absolute bottom-0 left-0 w-full">
-      <form className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
+      <div className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
         <div className="relative flex h-full flex-1 items-stretch md:flex-col">
           <div className="relative flex flex-col w-full flex-grow p-4">
             <div className="relative">
@@ -25,6 +25,8 @@ const ChatInput = ({ isDisabled }: ChatInputProps) => {
                 ref={textareaRef}
                 maxRows={4}
                 autoFocus
+                onChange={handleInputChange}
+                value={message}
                 onKeyDown={(e) => {
                   if (e.key === "Enter" && !e.shiftKey) {
                     e.preventDefault();
@@ -34,28 +36,26 @@ const ChatInput = ({ isDisabled }: ChatInputProps) => {
                     textareaRef.current?.focus();
                   }
                 }}
-                onChange={handleInputChange}
-                value={message}
                 placeholder="Enter your question..."
                 className="resize-none pr-12 text-base py-3 scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
               />
 
               <Button
+                disabled={isLoading || isDisabled}
                 className="absolute bottom-1.5 right-[8px]"
                 aria-label="send message"
-                disabled={isLoading || isDisabled}
-                onClick={(e) => {
-                  e.preventDefault();
+                onClick={() => {
                   addMessage();
+
                   textareaRef.current?.focus();
                 }}
-                >
+              >
                 <Send className="h-4 w-4" />
               </Button>
             </div>
           </div>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -1,51 +1,76 @@
-import { trpc } from "@/app/_trpc/client"
-import { INFINITE_QUERY_LIMIT } from "@/config/infinite-query"
-import { keepPreviousData } from "@tanstack/react-query"
-import { Loader2, MessageSquare } from "lucide-react"
-import Message from "@/components/chat/Message"
-import Skeleton from "react-loading-skeleton"
+import { trpc } from "@/app/_trpc/client";
+import { INFINITE_QUERY_LIMIT } from "@/config/infinite-query";
+import { Loader2, MessageSquare } from "lucide-react";
+import Skeleton from "react-loading-skeleton";
+import Message from "./Message";
+import { useContext, useEffect, useRef } from "react";
+import { ChatContext } from "./ChatContext";
+import { keepPreviousData } from "@tanstack/react-query";
 
-const Messages = ({fileId}:{fileId:string}) => {
+interface MessagesProps {
+  fileId: string;
+}
 
-  const { data, isLoading, fetchNextPage } = trpc.getFileMessages.useInfiniteQuery({
-    fileId,
-    limit: INFINITE_QUERY_LIMIT
-  }, {
-    getNextPageParam: (lastPage) => lastPage?.nextCursor,
-    placeholderData: keepPreviousData
-  })
+const Messages = ({ fileId }: MessagesProps) => {
+  const { isLoading: isAiThinking } = useContext(ChatContext);
 
-  const messages = data?.pages.flatMap(page => page.messages);
+  const { data, isLoading, fetchNextPage } =
+    trpc.getFileMessages.useInfiniteQuery(
+      {
+        fileId,
+        limit: INFINITE_QUERY_LIMIT,
+      },
+      {
+        getNextPageParam: (lastPage) => lastPage?.nextCursor,
+        placeholderData: keepPreviousData
+      }
+    );
+
+  const messages = data?.pages.flatMap((page) => page.messages);
 
   const loadingMessage = {
     createdAt: new Date().toISOString(),
-    id: "loading message",
+    id: "loading-message",
     isUserMessage: false,
     text: (
       <span className="flex h-full items-center justify-center">
-        <Loader2 className="h-4 w-4 animate-spin"/>
+        <Loader2 className="h-4 w-4 animate-spin" />
       </span>
-    )
-  }
+    ),
+  };
 
   const combinedMessages = [
-    ...(true ? [loadingMessage] : []),
-    ...(messages ?? [])
-  ]
+    ...(isAiThinking ? [loadingMessage] : []),
+    ...(messages ?? []),
+  ];
+
+  const lastMessageRef = useRef<HTMLDivElement>(null);
+
 
   return (
     <div className="flex max-h-[calc(100vh-3.5rem-7rem)] border-zinc-200 flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
       {combinedMessages && combinedMessages.length > 0 ? (
         combinedMessages.map((message, i) => {
-          const isNextMessagesSamePerson =
+          const isNextMessageSamePerson =
             combinedMessages[i - 1]?.isUserMessage ===
-            combinedMessages[i].isUserMessage;
+            combinedMessages[i]?.isUserMessage;
 
           if (i === combinedMessages.length - 1) {
-            return <Message />;
-          } else {
-            return <Message />;
-          }
+            return (
+              <Message
+                message={message}
+                isNextMessageSamePerson={isNextMessageSamePerson}
+                key={message.id}
+              />
+            );
+          } else
+            return (
+              <Message
+                message={message}
+                isNextMessageSamePerson={isNextMessageSamePerson}
+                key={message.id}
+              />
+            );
         })
       ) : isLoading ? (
         <div className="w-full flex flex-col gap-2">
@@ -65,6 +90,6 @@ const Messages = ({fileId}:{fileId:string}) => {
       )}
     </div>
   );
-}
+};
 
-export default Messages
+export default Messages;
